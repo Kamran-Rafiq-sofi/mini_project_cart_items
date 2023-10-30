1ee1caa2c4f2973ef6b2c4b22c3889f601d9e9c9
Kamran-Rafiq-Sofi <kamranrafiq805@gmail.com>
2023-10-30 18:41:24 +0530

    stylrs added to cart items

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Cartitem from "./Cartitem";
 
 function App() {
   return (
     <div className="App">
       <h1>Cart_Items</h1>
+      <Cartitem/>
     </div>
   );
 }
