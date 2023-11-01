ddd078ac2cbbc00adbe953a98a87ed7cd51b896b
Kamran-Rafiq-Sofi <kamranrafiq805@gmail.com>
2023-11-01 22:05:48 +0530

    added increase and decrease functions

--- src/Cartitem.js
+++ src/Cartitem.js
@@ -1,40 +1,76 @@
 import React from "react";
 class Cartitem extends React.Component {
-     constructor(){
+    constructor() {
         super();
-        this.state={
-            title:"Mobile Phone",
-            price:999,
-            qty:1,
-            img:''
+        this.state = {
+            title: "Mobile Phone",
+            price: 999,
+            qty: 1,
+            img: ''
 
 
         }
-        this.increaseQuantity=this.increaseQuantity.bind(this);
-     }
-    //  increaseQuantity(){
-    //     //    qty= qty+1;
-    //     alert("windows")
-    //     console.log("this.state",this.state) // error
-    //     console.log("this",this) //undefined
-
-    //         }
-
-    increaseQuantity=()=>{
+        // this.increaseQuantity=this.increaseQuantity.bind(this);
+    }
+    /*
+     increaseQuantity(){
         //    qty= qty+1;
         alert("windows")
         console.log("this.state",this.state) // error
         console.log("this",this) //undefined
 
             }
+    */
+    increaseQuantity = () => {
+        // alert("windows")
+        console.log("this.state", this.state) // error
+        // console.log("this", this) //undefined
+
+        /*
+            //one way but it wont be rendered
+                 this.state.qty+=1;
+        */
+
+        /*
+          //one way is using concept of object using setState property use if prev value noy used
+             this.setState(
+                    {
+                        qty:this.state.qty+1
+                    }
+                );
+        */
+        
+          // 2nd way is using function concept use this if previous value is needed
+           this.setState((prevState)=>{
+            return{
+                qty:prevState.qty+1
+            }
+           }); 
+        
+    }
+
+
+    decreaseQuantity = () => {
+        if(this.state.qty>0){
+
+           this.setState((prevState)=>{
+            return{
+                qty:prevState.qty-1
+            }
+           }); 
+        
+    }
+    
+    
+}
     render() {
         // destructuring
-        const {title, price, qty}=this.state;
-        
-        return(
+        const { title, price, qty } = this.state;
+
+        return (
             <div className="cart-item">
                 <div className="left-block">
-                    <img alt=""style={styles.image}/>
+                    <img alt="" style={styles.image} />
                 </div>
                 <div className="right-block">
                     {/* <div style={{fontSize:25}}>Title</div>
@@ -43,29 +79,30 @@ class Cartitem extends React.Component {
                     {/* <div style={{fontSize:25}}>{this.state.title}</div>
                     <div style={{color:'#777'}}>Rs:{this.state.price}</div>
                     <div style={{color:'#777'}}>Qty:{this.state.qty}</div> */}
-                    <div style={{fontSize:25}}>{title}</div>
-                    <div style={{color:'#777'}}>Rs:{price}</div>
-                    <div style={{color:'#777'}}>Qty:{qty}</div>
+                    <div style={{ fontSize: 25 }}>{title}</div>
+                    <div style={{ color: '#777' }}>Rs:{price}</div>
+                    <div style={{ color: '#777' }}>Qty:{qty}</div>
                     <div className="cart-item-actions">
-                       <img alt="increase" 
-                       className="action-icons" 
-                       src="https://cdn-icons-png.flaticon.com/128/992/992651.png" 
-                    //    onClick={this.increaseQuantity}
-                    // // one way
-                    //    onClick={this.increaseQuantity.bind(this)}
+                        <img alt="increase"
+                            className="action-icons"
+                            src="https://cdn-icons-png.flaticon.com/128/992/992651.png"
+                            //    onClick={this.increaseQuantity}
+                            // // one way
+                            //    onClick={this.increaseQuantity.bind(this)}
 
-                    onClick={this.increaseQuantity}
+                            onClick={this.increaseQuantity}
+                        />
 
+                        <img alt="decrease"
+                            className="action-icons"
+                            src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png"
+                            onClick={this.decreaseQuantity}
+                        />
 
-                       />
-
-                       <img alt="decrease" 
-                       className="action-icons" 
-                       src="https://cdn-icons-png.flaticon.com/128/1828/1828906.png"/>
-
-                       <img alt="delete" 
-                       className="action-icons" 
-                       src="https://cdn-icons-png.flaticon.com/128/1214/1214428.png"/>
+                        <img alt="delete"
+                            className="action-icons"
+                            src="https://cdn-icons-png.flaticon.com/128/1214/1214428.png" 
+                        />
                     </div>
                 </div>
             </div>
@@ -74,13 +111,13 @@ class Cartitem extends React.Component {
     }
 }
 // styling using object type
-const styles={
-    image:{
-        height:110,
-        width:110,
-        borderRadius:4,
+const styles = {
+    image: {
+        height: 110,
+        width: 110,
+        borderRadius: 4,
         // padding:10,
-        background:'#ccc'
+        background: '#ccc'
     }
 }
 export default Cartitem;
